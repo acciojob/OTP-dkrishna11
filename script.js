1e81166a5496fe1ccd7775bb98fb40e6008ce60b
dkrishna11 <116519809+dkrishna11@users.noreply.github.com>
2023-05-28 12:31:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const codes = document.getElementsByClassName('otp');
+
+    for (let i = 0; i < codes.length; i++) {
+      codes[i].addEventListener('input', function() {
+        if (this.value.length === this.maxLength) {
+          if (i < codes.length - 1) {
+            codes[i + 1].focus();
+          }
+        }
+      });
+
+      codes[i].addEventListener('keydown', function(event) {
+        if (event.key === 'Backspace' && this.value.length === 0) {
+          if (i > 0) {
+            codes[i - 1].focus();
+          }
+        }
+      });
+    }
